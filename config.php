4b90e962063c8f57266f359af0d0aee42d2835f1
jvictoroliver <141880858+jvictoroliver@users.noreply.github.com>
2023-11-21 19:50:25 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -4,8 +4,4 @@ define('USER', 'root'); // Nome de usuário do banco de dados
 define('PASS', 'minas'); // Senha do banco de dados
 define('BASE', 'bd_escola'); // Nome do banco de dados
 $conn = new mysqli(HOST, USER, PASS, BASE);
-
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-}
 ?>
